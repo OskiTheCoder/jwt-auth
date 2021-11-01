49f82df2ca0ffbaaf962de6221abb8980fbe07bf
shelvy <sks91@berkeley.edu>
2021-11-01 15:04:55 -0700

    password handling, error handling, and partials

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,23 @@
+const User = require('../models/User');
+
+//error handling
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+  //validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
@@ -6,12 +26,19 @@ module.exports.login_get = (req, res) => {
   res.render('login');
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send('new signup');
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   res.send('new login');
 };
